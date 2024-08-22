2e8460b8443df633127fc4a69968c9af3bf6b86d
vanispurtree1111 <vani.kn@spurtreetech.com>
2024-08-22 15:15:46 +0530

    custom component added

--- Tracker/src/main.ts
+++ Tracker/src/main.ts
@@ -5,3 +5,6 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  // first code file to be executed when ur angular file loads up in browsers of the visitors
